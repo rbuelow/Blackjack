bdf909941f3f119992666c83bd2bd5aaf0c43015
chad rehm <rehmchad@yahoo.com>
2015-11-15 11:28:03 -0600

    added theDeck.js
    added the dictionary and added the deck as string representations.
    changed the html and css a little

--- javascript/shuffle.js
+++ javascript/shuffle.js
@@ -1,10 +1,61 @@
-function load(){
-  var array =  new Array()
-  for(j=1;j<53;j++){
-    array.push(j);
-  }
-  shuffle(array);
-}
+$(".load").click(function load(){
+var deck = [
+  "hA", 
+  "h2", 
+  "h3", 
+  "h4", 
+  "h5", 
+  "h6", 
+  "h7", 
+  "h8", 
+  "h9", 
+  "h10", 
+  "hJ", 
+  "hQ", 
+  "hK", 
+  "dA", 
+  "d2", 
+  "d3", 
+  "d4", 
+  "d5", 
+  "d6", 
+  "d7", 
+  "d8", 
+  "d9", 
+  "d10", 
+  "dJ", 
+  "dQ", 
+  "dK", 
+  "cA",
+  "c2", 
+  "c3", 
+  "c4", 
+  "c5", 
+  "c6", 
+  "c7", 
+  "c8", 
+  "c9", 
+  "c10", 
+  "cJ", 
+  "cQ",
+  "cK", 
+  "sA",
+  "s2", 
+  "s3", 
+  "s4", 
+  "s5", 
+  "s6", 
+  "s7", 
+  "s8", 
+  "s9",
+  "s10", 
+  "sJ", 
+  "sQ",
+  "sK", 
+]
+  shuffle(deck);
+});
+
 function shuffle(array) {
   var m = array.length, t, i;
 
@@ -22,14 +73,27 @@ function shuffle(array) {
   document.getElementById("deckdisplay").innerHTML = array;
   localStorage["deck"] = JSON.stringify(array);
 }
-function pullCard(count){
+
+$(".draw").click(function pullCard(){
   var deck = JSON.parse(localStorage["deck"]);
   var card = deck.pop();
+  var stringCard = card.toString();
   localStorage["deck"] = JSON.stringify(deck);
-  document.getElementById("cardDisplay"+count).innerHTML = card;
+  $(".dealer").append("<li  class=&#32;cardDisplay&#32;></li>")
+  $("li:last").append(stringCard);
+});
+
+$(".deal").click(function pullCard(){
+  var deck = JSON.parse(localStorage["deck"]);
+  for (var x = 0; x<2; x++){
+  var cardOne = deck.pop();
+  var cardTwo = deck.pop();
+  var stringCardOne = cardOne.toString();
+  var stringCardTwo = cardTwo.toString();
+  localStorage["deck"] = JSON.stringify(deck);
+  $(".dealer").append("<li  class=&#32;cardDisplay&#32;></li>")
+  $(".dealer li:last").append(stringCardOne);
+  $(".player").append("<li  class=&#32;cardDisplay&#32;></li>")
+  $(".player li:last").append(stringCardTwo);
 }
-count = 1;
-function draw(){
-  pullCard(count);
-  count++;
-}
+});
